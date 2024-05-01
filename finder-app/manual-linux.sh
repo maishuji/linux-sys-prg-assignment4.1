34c301d3b6c0406dbcef3ed8e9afa071ab883650
maishuji <daoliangshumaishuji@gmail.com>
2024-05-01 23:34:09 +0200

    Add step to build image kernel

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -50,6 +50,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     # DONE: Add your kernel build steps here
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}  defconfig
+    make -j4 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
