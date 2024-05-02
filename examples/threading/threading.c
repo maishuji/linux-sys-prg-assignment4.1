43ff58a49f6302f5cf1b23e3963b0dd0ea0a066f
maishuji <daoliangshumaishuji@gmail.com>
2024-05-02 14:58:45 +0200

    Asignment 4.1 : Working with threads

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -11,9 +11,31 @@
 void* threadfunc(void* thread_param)
 {
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
+    // DONE: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thdata = (struct thread_data*)thread_param;
+    usleep(thdata->wait_to_obtain_ms);
+
+    int ret = pthread_mutex_lock(thdata->mutex);
+    if(ret == 0){
+        //Critical section
+
+        usleep(thdata->wait_to_release_ms);
+
+        //End critical section
+        ret = pthread_mutex_unlock(thdata->mutex);
+        if(ret != 0){
+            ERROR_LOG("Failed to release mutex");
+            thdata->thread_complete_success = false;
+        }else{
+            thdata->thread_complete_success = true; // Success
+        }
+
+    }else{
+        ERROR_LOG("Failed to acquire mutex");
+        thdata->thread_complete_success = false;
+    }
     return thread_param;
 }
 
@@ -21,13 +43,20 @@ void* threadfunc(void* thread_param)
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
     /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
+     * DONE: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
      * using threadfunc() as entry point.
      *
      * return true if successful.
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *thdata = malloc(sizeof(struct thread_data));
+    thdata->mutex = mutex;
+    thdata->wait_to_obtain_ms = wait_to_obtain_ms;
+    thdata->wait_to_release_ms = wait_to_release_ms;
+    thdata->thread_complete_success = false; // true on success
+
+    int ret = pthread_create(thread, NULL, &threadfunc, thdata);
+    return ret != 0? false : true;
 }
 
